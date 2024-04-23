14e5b288ce73402d920748af79f83e083f4e1b87
anil <anil.kumar.ait09@gmail.com>
2024-04-23 15:28:55 +1000

    polestar-konnect  directory changes

--- src/main/java/com/polestar/konnect/constant/Constant.java
+++ src/main/java/com/polestar/konnect/constant/Constant.java
@@ -1,10 +1,11 @@
 package com.polestar.konnect.constant;
 
 public class Constant {
-    public static final String CRIMINALS_PATH = "/usr/local/polestar/result/captured-criminals/";
-    public static final String CRIMINALS_ARCHIVE_PATH = "/usr/local/polestar/data/archives/captured-criminals/";
-    public static final String FRIENDS_PATH = "/usr/local/polestar/result/known-visitors/";
-    public static final String FRIENDS_ARCHIVE_PATH = "/usr/local/polestar/data/archives/known-visitors/";
-    public static final String VISITOR_PATH = "/usr/local/polestar/result/unknown-visitors/";
-    public static final String VISITOR_ARCHIVE_PATH = "/usr/local/polestar/data/archives/unknown-visitors/";
+    public static final String CRIMINALS_PATH = "/usr/local/polestar/detections/captured-criminals/";
+    public static final String FRIENDS_PATH = "/usr/local/polestar/detections/familiar-faces/";
+    public static final String VISITOR_PATH = "/usr/local/polestar/detections/unknown-visitors/";
+
+    public static final String CRIMINALS_ARCHIVE_PATH = "/usr/local/polestar/detections/archives/captured-criminals/";
+    public static final String FRIENDS_ARCHIVE_PATH = "/usr/local/polestar/detections/archives/familiar-faces/";
+    public static final String VISITOR_ARCHIVE_PATH = "/usr/local/polestar/detections/archives/unknown-visitors/";
 }
